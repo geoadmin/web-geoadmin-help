f924d84dce35d03e607469459fea263c204ce936
Andrea Borghi <andrea.borghi@swisstopo.ch>
2015-09-22 09:31:29 +0200

    added modifications from ltfap

---
+++ htdocs/js_css/accordion_menu.js
@@ -0,0 +1,69 @@
+function menu1() {
+ $('.submenu1').animate({"height": 'toggle'});
+ $('.submenu2').hide();
+ $('.submenu3').hide();
+ $('.submenu4').hide();
+ $('.submenu5').hide();
+ $('.submenu6').hide();
+ $('.submenu7').hide();
+}
+
+function menu2() {
+ $('.submenu1').hide();
+ $('.submenu2').animate({"height": 'toggle'});
+ $('.submenu3').hide();
+ $('.submenu4').hide();
+ $('.submenu5').hide();
+ $('.submenu6').hide();
+ $('.submenu7').hide();
+
+}
+function menu3() {
+ $('.submenu1').hide();
+ $('.submenu2').hide();
+ $('.submenu3').animate({"height": 'toggle'});
+ $('.submenu4').hide();
+ $('.submenu5').hide();
+ $('.submenu6').hide();
+ $('.submenu7').hide();
+}
+
+function menu4() {
+ $('.submenu1').hide();
+ $('.submenu2').hide();
+ $('.submenu3').hide();
+ $('.submenu4').animate({"height": 'toggle'});
+ $('.submenu5').hide();
+ $('.submenu6').hide();
+ $('.submenu7').hide();
+}
+
+function menu5() {
+ $('.submenu1').hide();
+ $('.submenu2').hide();
+ $('.submenu3').hide();
+ $('.submenu4').hide();
+ $('.submenu5').animate({"height": 'toggle'});
+ $('.submenu6').hide();
+ $('.submenu7').hide();
+}
+
+function menu6() {
+ $('.submenu1').hide();
+ $('.submenu2').hide();
+ $('.submenu3').hide();
+ $('.submenu4').hide();
+ $('.submenu5').hide();
+ $('.submenu6').animate({"height": 'toggle'});
+ $('.submenu7').hide();
+}
+
+function menu7() {
+ $('.submenu1').hide();
+ $('.submenu2').hide();
+ $('.submenu3').hide();
+ $('.submenu4').hide();
+ $('.submenu5').hide();
+ $('.submenu6').hide();
+ $('.submenu7').animate({"height": 'toggle'});
+}
