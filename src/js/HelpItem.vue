eddddb9db9255bf5a6e4ac43efef8e31ab2a4e92
Pascal Barth <pak.barth@gmail.com>
2019-11-06 14:29:28 +0100

    add image to help item HTML

--- src/js/HelpItem.vue
+++ src/js/HelpItem.vue
@@ -2,6 +2,7 @@
     <div class="help-item">
         <h2 v-if="!item.isHomepage()">{{ item.title }}</h2>
         <div v-html="item.content"></div>
+        <img v-if="item.image" v-bind:alt="item.title" v-bind:src="item.image" />
     </div>
 </template>
 <script>
