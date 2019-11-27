d1ccf7a06bf845dadaaacce80e1ad5a9e7f33e3f
Pascal Barth <pak.barth@gmail.com>
2019-11-27 11:10:21 +0100

    Adjusting content margin

--- src/js/HelpItem.vue
+++ src/js/HelpItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="help-item" v-bind:id="'help-' + item.id">
         <h2 v-if="!item.isHomepage()">{{ item.title }}</h2>
-        <div v-html="item.content"></div>
+        <p v-html="item.content"></p>
         <img v-if="item.image" v-bind:alt="item.title" v-bind:src="item.image" />
     </div>
 </template>
